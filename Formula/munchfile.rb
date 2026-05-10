cc4b96a970db1941050b019fa5940576c3d9ee76
Ethan Buntario <ebuntario@gmail.com>
2026-05-10 21:19:46 +0700

    munchfile 0.3.0 — Phase 2 video pipeline

--- Formula/munchfile.rb
+++ Formula/munchfile.rb
@@ -1,26 +1,26 @@
 class Munchfile < Formula
   desc "Watch local files, share live URLs"
   homepage "https://github.com/ebuntario/munchfile-cli"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-arm64.tar.gz"
-      sha256 "afdee7845f2248acfc32f0c72b4a3bee97d7e54a01ff803a9454020c80778d82"
+      sha256 "7374ec804a13f36b333b830ec82c03ef542ab42b4e79394511ea1113e4702bc4"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-x64.tar.gz"
-      sha256 "e599ce8aae702b0d2d588d6e4101033ac92546721bc326a7a24420564564914a"
+      sha256 "1c8f5904399799a254128a4be95c3a4ae1ae154ce84f33d44ec4869eab920303"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-arm64.tar.gz"
-      sha256 "e3f228c1b8d73a19da6be80a4c1f4fe42b927f3d39cae40dfe817451d3069a76"
+      sha256 "85662dcf49ccb15eb4c598908420749f3e7a9b21c85ebb2f4d99d6dd79775909"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-x64.tar.gz"
-      sha256 "66adb59124f4ad268fed8a3ea4a85f0ac82e196b1760c9a8b36629ca67fc2017"
+      sha256 "0a75fb199204979e06fbe6bfc7ffd29ec0d8baa0ea960694ff6bc87ea2ee25b4"
     end
   end
 
