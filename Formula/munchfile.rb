dede79d935436a947f402e5803f4841ed5607679
Ethan Buntario <ebuntario@gmail.com>
2026-05-10 21:58:21 +0700

    munchfile 0.3.2 — fix single-file watch media boundary

--- Formula/munchfile.rb
+++ Formula/munchfile.rb
@@ -1,26 +1,26 @@
 class Munchfile < Formula
   desc "Watch local files, share live URLs"
   homepage "https://github.com/ebuntario/munchfile-cli"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-arm64.tar.gz"
-      sha256 "9411ee959a04679185122d0ff5f0e97b62d5dab9c49f04a3003a239818c47945"
+      sha256 "a2163c0a257c914e6985774cd943354360ff137267d62247544a702cd922d91e"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-x64.tar.gz"
-      sha256 "d31de2f3d39495edb596738bd6a3058eef52311dfabd6363da5b580145921483"
+      sha256 "dabaccf36ed621c43c65152d354d43a988efa6c4fa2277554da02f5677fb584f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-arm64.tar.gz"
-      sha256 "0b220615c73a73285f112adc245c16395340a11141deff05f58adca265d6e3e7"
+      sha256 "4a70ffc6aad21a195870b1958757672aaa9b7266ed58cab9d9fef185ee7793cc"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-x64.tar.gz"
-      sha256 "0346766ef72425f8a36291f5648358e7c2b78756b62eeaeb6152d9b510a093e9"
+      sha256 "887aa74db66ae49f1c45c840d126ede3c3c58fca7cfb0e11b9786b278375110c"
     end
   end
 
