6822131779ab1492bd1c6c42f466e204015e887e
Ethan Buntario <ebuntario@gmail.com>
2026-05-10 22:06:57 +0700

    munchfile 0.3.3 — image bodyLimit fix + ffprobe ENOENT graceful skip

--- Formula/munchfile.rb
+++ Formula/munchfile.rb
@@ -1,26 +1,26 @@
 class Munchfile < Formula
   desc "Watch local files, share live URLs"
   homepage "https://github.com/ebuntario/munchfile-cli"
-  version "0.3.2"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-arm64.tar.gz"
-      sha256 "a2163c0a257c914e6985774cd943354360ff137267d62247544a702cd922d91e"
+      sha256 "979fbef78f37fe540ea1eef176f6cb8c53bf5908133ab8ad9198b8c44e6d3fa4"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-darwin-x64.tar.gz"
-      sha256 "dabaccf36ed621c43c65152d354d43a988efa6c4fa2277554da02f5677fb584f"
+      sha256 "02ad7fd3759f3ebc9860f045c9ef3f6704c646f91d0b7257939ce80ca316d424"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-arm64.tar.gz"
-      sha256 "4a70ffc6aad21a195870b1958757672aaa9b7266ed58cab9d9fef185ee7793cc"
+      sha256 "066ae14c0b9df19ceb8248c501f4f3a4ce34426a83caaf26ae084fdb8752f618"
     else
       url "https://github.com/ebuntario/munchfile-cli/releases/download/v#{version}/munchfile-#{version}-linux-x64.tar.gz"
-      sha256 "887aa74db66ae49f1c45c840d126ede3c3c58fca7cfb0e11b9786b278375110c"
+      sha256 "c33a7d30cd5d4e2e2daeecbf56b1a3555f42678f0d1b52a41e40e245d367d85c"
     end
   end
 
